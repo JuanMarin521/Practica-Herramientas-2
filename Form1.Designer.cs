5817847b41b2c65c03806ab7eb3bd45ab8921ae3
Juan Marin <juanpablomarin766@gmail.com>
2025-02-25 21:26:00 -0500

    Mi primer commit...

---
+++ Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace FormPractica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nombreBox = new System.Windows.Forms.TextBox();
+            this.nombreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nombreBox
+            // 
+            this.nombreBox.Location = new System.Drawing.Point(228, 89);
+            this.nombreBox.Name = "nombreBox";
+            this.nombreBox.Size = new System.Drawing.Size(100, 20);
+            this.nombreBox.TabIndex = 0;
+            // 
+            // nombreLabel
+            // 
+            this.nombreLabel.AutoSize = true;
+            this.nombreLabel.Location = new System.Drawing.Point(125, 96);
+            this.nombreLabel.Name = "nombreLabel";
+            this.nombreLabel.Size = new System.Drawing.Size(44, 13);
+            this.nombreLabel.TabIndex = 1;
+            this.nombreLabel.Text = "Nombre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nombreLabel);
+            this.Controls.Add(this.nombreBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nombreBox;
+        private System.Windows.Forms.Label nombreLabel;
+    }
+}
+
